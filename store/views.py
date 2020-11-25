59d8e9086eea7e6bbc30b9d56b67420c34fa1991
Boat-Programmer <53931555+Boat-Programmer@users.noreply.github.com>
2020-11-25 19:08:28 +0700

    updata templates

--- store/views.py
+++ store/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+import datetime
 
 # Create your views here.
+
+def index(request):
+    today = datetime.datetime.now().date()
+    return render(request, 'index.html', {"today" : today})
